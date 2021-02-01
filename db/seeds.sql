03554263f6f98f061a792ac16c62a532a3ae760c
Slogreco <shoshwright@hotmail.com>
2021-02-01 13:32:30 -0700

    created schema and seeds in db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Protien Style');
+INSERT INTO burgers (burger_name) VALUES ('Mac Burger');
+INSERT INTO burgers (burger_name) VALUES ('El Diablo Burger');
+INSERT INTO burgers (burger_name) VALUES ('BYOB Burger');
