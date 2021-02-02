cfb93ec07824fe21e8fc7a9755926f2e1d76d898
Slogreco <shoshwright@hotmail.com>
2021-02-02 07:01:29 -0700

    created burger model and controller

---
+++ controllers/burger-controller.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const burger = require("../models/burger.js");
+
+
+
+
+module.exports = router; 
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+
+};
+
+module.exports = burger
