732e382c4a79e0a83f490bfaf8e09eb3d8d93ef7
Slogreco <shoshwright@hotmail.com>
2021-02-02 06:53:58 -0700

    updated connection to be compatible with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Claptonrules77!!",
+        database: "burger_db"
+    })
+};
 
 
 connection.connect(function (err) {
