ab9fc6638190f400619216f17b5eea0cff8164b4
Slogreco <shoshwright@hotmail.com>
2021-02-02 06:52:28 -0700

    created orm.js and connection.js to connect to local sql

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+});
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("connection error:" + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection
---
+++ config/orm.js
@@ -0,0 +1,9 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+    //function to select all data
+    // fuction to insert data
+    // function to update data
+};
+
+module.exports = orm; 
